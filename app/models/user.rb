a6ea374e389629e75583f43dc28425be44a5ceee
WeiLeen Ng <ngweileen@yahoo.co.uk>
2013-05-02 15:54:51 +0800

    Devise was throwing error undefined method encrypted_password, it was fixed with rake db migrate reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :confirmable, :lockable, :timeoutable and :omniauthable, :encryptable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
--- db/migrate/20130428135031_devise_create_users.rb
+++ db/migrate/20130428135031_devise_create_users.rb
@@ -33,8 +33,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      ## Token authenticatable
-      # t.string :authentication_token
+      # Token authenticatable
+      t.string :authentication_token
 
 
       t.timestamps
